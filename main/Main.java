e25f4022cdbd66017cac722e1c13a37d97dea1a5
infinitesnow <infinitesnow@MacbookPro.infinitesnow.duckdns.org>
2017-01-10 00:40:01 +0100

    First commit

---
+++ main/Main.java
@@ -0,0 +1,13 @@
+import com.twitter.hbc.example.SampleStreamExample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			SampleStreamExample.run("MxvSndRjOut2iEAoxztJGgF8R","XlcR6M5kGYOJ140OXtJKDObxmXI6HI2h5oXaGqIL1CvRGxz8iU","818556679172263936-6nvJxCzZMd0jt73ZAsuBpU65SYZlr61","ZFKPhElQQlFZTMIaxUyBzxCTGT4bfPa1mXB01ihI3LK7j");
+		} catch (InterruptedException e) {
+			System.out.println(e);
+		}
+	}
+
+}
